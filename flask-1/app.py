a59f50f08c7f03d38dd4e7ee41d0b17690cb03f0
ik-dipanshu <dipanshupatel.880@gmail.com>
2026-09-11 19:46:05 +0530

    app.py

---
+++ flask-1/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'This is the about page.'
+
+@app.route('/contact')
+def contact(): 
+    return 'This is the contact page.'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
